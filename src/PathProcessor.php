9020df1ac05b36ea22ff133bf66614aaa8b3639d
Lauri Eskola <lauri.eskola@gmail.com>
2015-12-07 10:34:28 +0200

    Made subpathauto work with 8.0.0 and fixed the unit tests

--- src/PathProcessor.php
+++ src/PathProcessor.php
@@ -7,8 +7,11 @@
 
 namespace Drupal\subpathauto;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
 use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
+use Drupal\Core\Render\BubbleableMetadata;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -16,13 +19,41 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class PathProcessor implements InboundPathProcessorInterface, OutboundPathProcessorInterface {
 
+  /**
+   * The path processor.
+   *
+   * @var \Drupal\Core\PathProcessor\InboundPathProcessorInterface
+   */
   protected $pathProcessor;
 
   /**
-   * {@inheritdoc}
+   * The config factory
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
-  public function __construct(InboundPathProcessorInterface $path_processor) {
+  protected $configFactory;
+
+  /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * Builds PathProcessor object.
+   *
+   * @param \Drupal\Core\PathProcessor\InboundPathProcessorInterface $path_processor
+   *   The path processor.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   */
+  public function __construct(InboundPathProcessorInterface $path_processor, ConfigFactoryInterface $config_factory, LanguageManagerInterface $language_manager) {
     $this->pathProcessor = $path_processor;
+    $this->configFactory = $config_factory;
+    $this->languageManager = $language_manager;
   }
 
   /**
@@ -30,44 +61,44 @@ class PathProcessor implements InboundPathProcessorInterface, OutboundPathProces
    */
   public function processInbound($path, Request $request) {
     $request_path = $this->getPath($request->getPathInfo());
-
     if ($request_path !== $path) {
       return $path;
     }
     $original_path = $path;
     $subpath = array();
+    $path = ltrim($path, '/');
     while ($path_array = explode('/', $path)) {
       $subpath[] = array_pop($path_array);
       if (empty($path_array)) {
         break;
       }
       $path = implode('/', $path_array);
-      $processed_path = $this->pathProcessor->processInbound($path, $request);
+      $processed_path = $this->pathProcessor->processInbound('/' . $path, $request);
       if ($processed_path !== $path) {
         $path = $processed_path . '/' . implode('/', array_reverse($subpath));
         return $path;
       }
     }
-
     return $original_path;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function processOutbound($path, &$options = array(), Request $request = NULL) {
-    if ($options['absolute']) {
+  public function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleableMetadata = NULL) {
+    if (isset($options['absolute']) && $options['absolute']) {
       return $path;
     }
     $original_path = $path;
     $subpath = array();
+    $path = ltrim($path, '/');
     while ($path_array = explode('/', $path)) {
       $subpath[] = array_pop($path_array);
       if (empty($path_array)) {
         break;
       }
       $path = implode('/', $path_array);
-      $processed_path = $this->pathProcessor->processOutbound($path, $options, $request);
+      $processed_path = $this->pathProcessor->processOutbound('/' . $path, $options, $request);
       if ($processed_path !== $path) {
         $path = $processed_path . '/' . implode('/', array_reverse($subpath));
         return $path;
@@ -81,20 +112,22 @@ class PathProcessor implements InboundPathProcessorInterface, OutboundPathProces
    * Helper function to handle multilingual paths.
    *
    * @param string $path_info
-   *   path that might contain language prefix.
+   *   Path that might contain language prefix.
+   * @return string $path info
+   *   Path without language prefix.
    */
   protected function getPath($path_info) {
-    $config = \Drupal::config('language.negotiation')->get('url');
-    $parts = explode('/', ltrim($path_info, '/'));
-    $prefix = array_shift($parts);
-    $request_path = '';
-
-    // Search prefix within added languages.
-    foreach (\Drupal::languageManager()->getLanguages() as $language) {
-      if (isset($config['prefixes'][$language->getId()]) && $config['prefixes'][$language->getId()] == $prefix) {
-        // Rebuild $path with the language removed.
-        return implode('/', $parts);
-      }
+    // @todo Try to find a better solution. See also RedirectSubscriber checkRedirect().
+    $config = $this->configFactory->get('language.negotiation')->get('url');
+    $langcode = $this->languageManager->getCurrentLanguage()->getId();
+    $language_prefix = $config['prefixes'][$langcode];
+    $path_info = trim($path_info, '/');
+    if ($language_prefix) {
+      $parts = explode('/', $path_info);
+      array_shift($parts);
+      $path_info = implode('/', $parts);
     }
+    return '/' . $path_info;
   }
+
 }
--- tests/Drupal/subpathauto/Tests/SubPathautoTest.php
+++ tests/Drupal/subpathauto/Tests/SubPathautoTest.php
@@ -1,111 +1,131 @@
 <?php
 
 /**
- * @file
- * Contains \Drupal\subpathauto\Tests\SubPathautoTest.
- */
+* @file
+* Contains \Drupal\subpathauto\Tests\SubPathautoTest.
+*/
 
 namespace Drupal\subpathauto\Tests;
 
+use Drupal\Core\Language\Language;
 use Drupal\Tests\UnitTestCase;
-use Drupal\Core\PathProcessor\PathProcessorAlias;
-use Drupal\Core\PathProcessor\PathProcessorManager;
 use Symfony\Component\HttpFoundation\Request;
-
 use Drupal\subpathauto\PathProcessor;
 
 /**
- * Unit tests for Sub-Pathauto functaionality.
+ * Unit tests for Sub-Pathauto functionality.
  */
 class SubPathautoTest extends UnitTestCase {
 
-  protected $processorManager;
+  /**
+   * @var \Drupal\Core\PathProcessor\PathProcessorAlias|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $aliasProcessor;
 
   /**
-   * Function getInfo().
+   * @var \Drupal\Core\Config\ConfigFactoryInterface|\PHPUnit_Framework_MockObject_MockObject
    */
-  public static function getInfo() {
-    return array(
-      'name' => 'SubPathauto',
-      'description' => 'Confirm that the UrlGenerator is functioning properly.',
-      'group' => 'Routing',
-    );
+  protected $configFactory;
+
+  /**
+   * @var \Drupal\Core\Language\LanguageManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $languageManager;
+
+  /**
+   * The service under testing.
+   *
+   * @var \Drupal\subpathauto\PathProcessor
+   */
+  protected $sut;
+
+  public function setUp() {
+    parent::setUp();
+
+    $this->aliasProcessor = $this->getMockBuilder('Drupal\Core\PathProcessor\PathProcessorAlias')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->languageNegotiation = $this->getMockBuilder('Drupal\language\Annotation\LanguageNegotiation')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->configFactory = $this->getMock('Drupal\Core\Config\ConfigFactoryInterface');
+    $this->configFactory->expects($this->any())
+      ->method('get')
+      ->with('language.negotiation')
+      ->willReturn($this->languageNegotiation);
+
+    $this->languageManager = $this->getMock('Drupal\Core\Language\LanguageManagerInterface');
+    $this->languageManager->expects($this->any())
+      ->method('getCurrentLanguage')
+      ->willReturn(new Language());
+
+    $this->sut = new PathProcessor($this->aliasProcessor, $this->configFactory, $this->languageManager);
   }
 
   /**
    * Tests the basic functionality of the processInbound() method.
    */
   public function testInboundSubPath() {
-    $alias_processor = $this->getMockBuilder('Drupal\Core\PathProcessor\PathProcessorAlias')
-      ->disableOriginalConstructor()
-      ->getMock();
-    $alias_processor->expects($this->any())
+    $this->aliasProcessor->expects($this->any())
       ->method('processInbound')
-      ->will($this->returnCallback(array($this, 'pathAliasCallback')));
-    $subpath_processor = new PathProcessor($alias_processor);
+      ->will($this->returnCallback([$this, 'pathAliasCallback']));
 
     // Look up a subpath of the 'content/first-node' alias.
-    $processed = $subpath_processor->processInbound('content/first-node/a', Request::create('content/first-node/a'));
-    $this->assertEquals('node/1/a', $processed);
+    $processed = $this->sut->processInbound('/content/first-node/a', Request::create('content/first-node/a'));
+    $this->assertEquals('/node/1/a', $processed);
 
     // Look up a subpath of the 'content/first-node-test' alias.
-    $processed = $subpath_processor->processInbound('content/first-node-test/a', Request::create('content/first-node-test/a'));
-    $this->assertEquals('node/1/test/a', $processed);
+    $processed = $this->sut->processInbound('/content/first-node-test/a', Request::create('content/first-node-test/a'));
+    $this->assertEquals('/node/1/test/a', $processed);
 
     // Look up an admin sub-path of the 'content/first-node' alias without
     // disabling sub-paths for admin.
-    $processed = $subpath_processor->processInbound('content/first-node/edit', Request::create('content/first-node/edit'));
-    $this->assertEquals('node/1/edit', $processed);
+    $processed = $this->sut->processInbound('/content/first-node/edit', Request::create('content/first-node/edit'));
+    $this->assertEquals('/node/1/edit', $processed);
 
     // Look up an admin sub-path without disabling sub-paths for admin.
-    $processed = $subpath_processor->processInbound('malicious-path/modules', Request::create('malicious-path/modules'));
-    $this->assertEquals('admin/modules', $processed);
+    $processed = $this->sut->processInbound('/malicious-path/modules', Request::create('malicious-path/modules'));
+    $this->assertEquals('/admin/modules', $processed);
 
     // @todo Add tests for the functionality to disallow admin subpaths once it
     // has been implemented.
   }
 
+
   /**
    * Tests the basic functionality of the processInbound() method.
    */
   public function testInboundAlreadyProcessed() {
-    $alias_processor = $this->getMockBuilder('Drupal\Core\PathProcessor\PathProcessorAlias')
-      ->disableOriginalConstructor()
-      ->getMock();
-    $alias_processor->expects($this->never())
-      ->method('processInbound');
-    $subpath_processor = new PathProcessor($alias_processor);
-
     // The subpath processor should ignore this and not pass it on to the
     // alias processor.
-    $processed = $subpath_processor->processInbound('node/1', Request::create('content/first-node'));
+    $processed = $this->sut->processInbound('node/1', Request::create('content/first-node'));
   }
+
   /**
-   * Return value callback for getSystemPath() method on the mock alias manager.
+   * Return value callback for the getSystemPath() method on the mock alias manager.
    *
-   * Ensures that by default the call to getPathAlias() will return the
-   * first argument that was passed in. We special-case the paths for
-   * which we wish it to return an actual alias.
+   * Ensures that by default the call to getPathAlias() will return the first argument
+   * that was passed in. We special-case the paths for which we wish it to return an
+   * actual alias.
    *
    * @return string
    */
   public function pathAliasCallback() {
     $args = func_get_args();
-    switch ($args[0]) {
-      case 'content/first-node':
-        return 'node/1';
-
-      case 'content/first-node-test':
-        return 'node/1/test';
-
-      case 'malicious-path':
-        return 'admin';
-
+    switch($args[0]) {
+      case '/content/first-node':
+        return '/node/1';
+      case '/content/first-node-test':
+        return '/node/1/test';
+      case '/malicious-path':
+        return '/admin';
       case '':
         return '<front>';
-
       default:
         return $args[0];
     }
   }
+
 }
